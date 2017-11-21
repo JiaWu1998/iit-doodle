07d591576918431afa6a84c40bd2740911f09f34
Jiaxing Wu (James) <jwu96@hawk.iit.edu>
2017-11-21 01:01:04 +0000

    not sure if running

--- static/js/save.js
+++ static/js/save.js
@@ -6,7 +6,7 @@ saveButton.addEventListener('click',saveImage);
  function saveImage(){
     var data = canvas.toDataURL();
     
-    // var request = new XMLHttpRequest();
+    var request = new XMLHttpRequest();
     
     // request.onreadystatechange = function(){
     //     if(request.readyState == 4 && request.status == 200){
@@ -15,8 +15,8 @@ saveButton.addEventListener('click',saveImage);
 //         }
 //     }
     
-//     request.open('POST', 'save.php', true);
-//     request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-//     request.send('img='+data);
+    request.open('POST', 'save.php', true);
+    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    request.send('img='+data);
     
  }
