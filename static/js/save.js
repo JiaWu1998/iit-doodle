5a38fc1633fd200b0a64dfc4f51e3d972118abb7
Jiaxing Wu (James) <jwu96@hawk.iit.edu>
2017-11-26 20:59:59 +0000

    try push

--- static/js/save.js
+++ static/js/save.js
@@ -1,21 +1,30 @@
 var canvas = document.getElementById('canvas');
 var saveButton = document.getElementById('submit');
 
-  database = firebase.database();
 
 saveButton.addEventListener('click',saveImage);
 
  function saveImage(){
+   // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCu-Dc7zMz7vkEaus-5yic_ZmdWgh46iuU",
+    authDomain: "iit-doodle-img.firebaseapp.com",
+    databaseURL: "https://iit-doodle-img.firebaseio.com",
+    projectId: "iit-doodle-img",
+    storageBucket: "iit-doodle-img.appspot.com",
+    messagingSenderId: "664829745808"
+  };
+  firebase.initializeApp(config);
 
+    var database = firebase.database();
+    var encodedImg = database.ref('pngs');
+    
     var key = canvas.toDataURL();
     
-    var ref = database.ref('drawing');
-    var data = {
-     name: "picture",
-     drawing: key
-    }
-    
-    ref.push(data);
+    var result = encodedImg.push({
+     name:'image',
+     base64: key
+    })
 
     // var request = new XMLHttpRequest();
     
