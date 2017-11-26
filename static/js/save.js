a7b8abb68bcaf77b15d296dc0caa06aa021738fa
Jiaxing Wu (James) <jwu96@hawk.iit.edu>
2017-11-26 06:32:43 +0000

    got 405 now

--- static/js/save.js
+++ static/js/save.js
@@ -6,12 +6,20 @@ saveButton.addEventListener('click',saveImage);
  function saveImage(){
 
     var data = canvas.toDataURL();
+    var final = 'img='+data;
 
     var request = new XMLHttpRequest();
     
-    request.open('POST', '/save.php', true);
+    request.onreadystatechange = function(){
+     if(request.readyState == 4 && request.status == 200){
+        var response = request.responseText;
+        window.open(response, '_blank', 'location=0, menubar=0');
+     }
+    }
+    
+    request.open('POST', 'save.php', true);
     request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    request.send('img='+data);
+    request.send(final);
     
  }
  
