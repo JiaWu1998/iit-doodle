2baff939179dff437a4236f3c17bc36ead08d145
Jiaxing Wu (James) <jwu96@hawk.iit.edu>
2017-11-26 20:31:52 +0000

    firebase done

--- static/js/save.js
+++ static/js/save.js
@@ -1,24 +1,43 @@
 var canvas = document.getElementById('canvas');
 var saveButton = document.getElementById('submit');
 
+  var config = {
+    apiKey: "AIzaSyCu-Dc7zMz7vkEaus-5yic_ZmdWgh46iuU",
+    authDomain: "iit-doodle-img.firebaseapp.com",
+    databaseURL: "https://iit-doodle-img.firebaseio.com",
+    projectId: "iit-doodle-img",
+    storageBucket: "iit-doodle-img.appspot.com",
+    messagingSenderId: "664829745808"
+  };
+  firebase.initializeApp(config);
+  database = firebase.database();
+
 saveButton.addEventListener('click',saveImage);
 
  function saveImage(){
 
-    var data = canvas.toDataURL();
-
-    var request = new XMLHttpRequest();
+    var key = canvas.toDataURL();
     
-    request.onreadystatechange = function(){
-     if(request.readyState == 4 && request.status == 200){
-        var response = request.responseText;
-        window.open(response, '_blank', 'location=0, menubar=0');
-     }
+    var ref = database.ref('drawing');
+    var data = {
+     name: "picture",
+     drawing: key
     }
     
-    request.open('POST', 'save.php', true);
-    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    request.send('img='+data);
+    ref.push(data);
+
+    // var request = new XMLHttpRequest();
+    
+    // request.onreadystatechange = function(){
+    //  if(request.readyState == 4 && request.status == 200){
+    //     var response = request.responseText;
+    //     window.open(response, '_blank', 'location=0, menubar=0');
+    //  }
+    // }
+    
+    // request.open('POST', 'save.php', true);
+    // request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    // request.send('img='+data);
     
  }
  
